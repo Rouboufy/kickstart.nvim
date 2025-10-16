4d70595576e944dae5222e1b81328a449065c6c4
Rouboufy <213021409+Rouboufy@users.noreply.github.com>
2025-10-16 11:53:28 +0900

    42 add

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,9 +1,30 @@
--- You can add your own plugins here or in other files in this directory!
---  I promise not to create any merge conflicts in this directory :)
---
--- See the kickstart.nvim README for more information
 return {
-  'ThePrimeagen/harpoon',
-  branch = 'harpoon2',
-  dependencies = { 'nvim-lua/plenary.nvim' },
+  -- Harpoon
+  {
+    'ThePrimeagen/harpoon',
+    branch = 'harpoon2',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+  },
+
+  -- 42 Header plugin (correct public repo)
+  {
+    'pbondoer/vim-42header',
+    config = function()
+      vim.g.user42 = 'blanglai'
+      vim.g.mail42 = 'blanglai@student.42.fr'
+    end,
+  },
+
+  {
+    'xeluxee/competitest.nvim',
+    dependencies = 'MunifTanjim/nui.nvim',
+    config = function()
+      require('competitest').setup {
+        -- Optional: define a custom command for norminette checking
+        run_command = {
+          c = 'norminette %s | grep -E "Error|Warning" || echo "Norm OK"',
+        },
+      }
+    end,
+  },
 }
